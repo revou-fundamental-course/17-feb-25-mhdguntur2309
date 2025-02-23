8a7e6d9adb35341c063e7f6eee6606d0ad0baca5
mhdguntur2309 <gunturmuhammad23@gmail.com>
2025-02-23 23:01:33 +0700

    update java script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 // ini javacript
+let nama, email, interest;        
+        function Validasi () {
+        nama = document.forms["formku"]["nama_anda"].value;
+        email = document.forms["formku"]["email"].value;
+        interest = document.forms["formku"]["interest"].value;
+        if (nama == "") {
+          alert("Name must be filled out");
+          return false;
+        }
+        if (email == "") {
+          alert("Email must be filled out");
+          return false;
+        }
+        if (interest == "") {
+          alert("Interest must be filled out");
+          return false;
+        }
+        console.log(nama);
+        console.log(email);
+        console.log(interest);
+
+};
