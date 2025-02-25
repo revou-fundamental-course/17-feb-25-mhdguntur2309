01cc8d15f27474fa6ccf151febb29874b5d1469f
mhdguntur2309 <gunturmuhammad23@gmail.com>
2025-02-25 22:19:53 +0700

    penambahan gambar

--- js/script.js
+++ js/script.js
@@ -4,20 +4,28 @@ let nama, email, interest;
         nama = document.forms["formku"]["nama_anda"].value;
         email = document.forms["formku"]["email"].value;
         interest = document.forms["formku"]["interest"].value;
-        if (nama == "") {
-          alert("Name must be filled out");
-          return false;
-        }
-        if (email == "") {
-          alert("Email must be filled out");
-          return false;
-        }
-        if (interest == "") {
-          alert("Interest must be filled out");
-          return false;
-        }
-        console.log(nama);
-        console.log(email);
-        console.log(interest);
 
+        if (nama == "" || email == "" || interest == "") {
+          alert("tidak boleh ada yang kosong");
+          return false;
+        }
+        else {
+          alert("Terima Kasih");
+          return false;
+        }
 };
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i, slides;
+  slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
